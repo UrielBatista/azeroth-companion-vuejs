a95d53dd11dac5df68e12fc652c1d3fe0cf1ece2
UrielBatista <ur.sabatista@gmail.com>
2025-02-28 15:23:04 -0300

    git push deploy

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,7 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
+  productionSourceMap: false,
   devServer: {
     allowedHosts: 'all'
   }
